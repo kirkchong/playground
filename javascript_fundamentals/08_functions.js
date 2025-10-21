7b782ee43669c02be13011a29684cd9d13507a65
Kirk <kirkchongwork@gmail.com>
2025-10-21 19:31:01 +0800

    function

---
+++ javascript_fundamentals/08_functions.js
@@ -0,0 +1,9 @@
+// procedures are functions in js
+function greetStudent(student) {
+    console.log(`helllo ${student.name}`) // interpolated string
+}
+
+function timeRemaining(timeElapsed, endTime) {
+    return endTime - timeElapsed;
+}
+
