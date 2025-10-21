b848589eecc3b64e2e86e799454b5609320866c3
Kirk <kirkchongwork@gmail.com>
2025-10-21 19:15:45 +0800

    variables

---
+++ javascript_fundamentals/04_variables.js
@@ -0,0 +1,9 @@
+let test = "test";
+// var test = "var";
+// will error out same var name
+
+var temp; // stores undefined 
+console.log(temp);
+
+var myarr = ["fren", "lst"];
+console.log(myarr.length);
