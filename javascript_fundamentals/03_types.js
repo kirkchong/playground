35e6c3b49c7563d5d8e87246e3e101e441c04077
Kirk <kirkchongwork@gmail.com>
2025-10-21 19:09:14 +0800

    types

---
+++ javascript_fundamentals/03_types.js
@@ -0,0 +1,12 @@
+typeof 43 // number
+
+typeof "kirk" // string
+
+typeof true //boolean
+
+typeof undefined // undefined
+
+typeof { age: 28 } // "object"
+
+typeof null // object, bug from 20 years ago apparently
+typeof [1,2,3] // object
