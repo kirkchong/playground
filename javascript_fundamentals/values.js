dc90d3d24dd1fd095bd0b1bd76da9451541e34b6
Kirk <kirkchongwork@gmail.com>
2025-10-21 18:59:40 +0800

    values

---
+++ javascript_fundamentals/values.js
@@ -0,0 +1,21 @@
+42
+3.14
+// No distinction between decimals and non decimals, both are numbers
+
+"String"
+// As per other languages
+
+true
+false
+// booleans
+
+null
+undefined
+// Have two ways due to historic reasons, code in a way it doesn't matter which
+
+[1, 2, 3]
+// standard array
+
+{ name: "kirk" }
+// Object as of now, do we not call this a dict
+// Key in quotes is optional, general is unquoted, sometimes quotes are needed
