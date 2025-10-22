07d2ab1f625145044baef3bdb52ccddcf9e3977c
Kirk <kirkchongwork@gmail.com>
2025-10-22 16:36:56 +0800

    function expression

---
+++ javascript_fundamentals/17_functionexpressions.js
@@ -0,0 +1,9 @@
+// Function is a first class value
+
+// anonymous function expression
+var clickHandler = function () { }
+var id = people.map(person => person.id)
+
+// named function expression
+var keyHandler = function keyHandler() { }
+var id = people.map(function getId(person) { return person.id })
